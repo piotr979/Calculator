5db2799ece8d3cce142990cd276cddbdb46b7328
Piotr Glaza <peterglaza@gmail.com>
2019-05-13 10:50:58 +0100

    secondary display working

---
+++ Calc/DatStorage.swift
@@ -0,0 +1,9 @@
+//
+//  DatStorage.swift
+//  Calc
+//
+//  Created by Piotr Glaza on 01/05/2019.
+//  Copyright © 2019 Piotr Glaza. All rights reserved.
+//
+
+import Foundation
---
+++ Calc/VIewControllerDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  VIewControllerDelegate.swift
+//  Calc
+//
+//  Created by Piotr Glaza on 05/05/2019.
+//  Copyright © 2019 Piotr Glaza. All rights reserved.
+//
+
+import Foundation

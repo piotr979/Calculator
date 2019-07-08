ed89cf376cd557b24c4893d8d2ff8d887a5db2ae
Piotr Glaza <peterglaza@gmail.com>
2019-07-08 12:25:05 +0100

    Highlight buttons

---
+++ Calc/View/CustomButton.swift
@@ -0,0 +1,21 @@
+//
+//  CustomButton.swift
+//  Calc
+//
+//  Created by Piotr Glaza on 08/07/2019.
+//  Copyright © 2019 Piotr Glaza. All rights reserved.
+//
+
+import UIKit
+
+class CustomButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
